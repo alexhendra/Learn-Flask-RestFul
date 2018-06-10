04f42518d7747bad03a31c96996b73a7a69c536c
Alex Hendra <alexhendrap@gmail.com>
2018-06-10 22:13:54 +0700

    Writing out Item resources to a database

--- app.py
+++ app.py
@@ -1,68 +1,19 @@
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
 from security import authenticate, identity
 
 from user import UserRegister
+from item import Item, ItemList
 
 app = Flask(__name__)
 app.secret_key = 'alexhp'
 api = Api(app)
 jwt = JWT(app, authenticate, identity)    # JWT akan membuatkan endpoint "/auth"
 
-items = []
-
-
-class Item(Resource):
-    # reqparse.RequestParser() dapat digunakan untuk memastikan data yang dikirimkan sudah sesuai, melalui validasi
-    parser = reqparse.RequestParser()
-    parser.add_argument('price',
-                        type=float,
-                        required=True,
-                        help='This field cannot be left blank!')
-
-    @jwt_required()
-    def get(self, name):
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        return {'item': item}, 200 if item else 404
-
-    def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None):
-            return {'message': "An item with name '{}' already exists.".format(name)}
-
-        # force=True; maksudnya agar request yang datang tidak mesti ada request header content-type : application/json
-        # data = request.get_json(force=True)
-
-        # data = request.get_json()
-        data = Item.parser.parse_args()
-        item = {'name': name, 'price': data['price']}
-        items.append(item)
-        return item, 201
-
-    def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message': 'Item deleted'}
-
-    def put(self, name):
-        # data = request.get_json()
-        data = Item.parser.parse_args()
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
-
-
-class ItemList(Resource):
-    def get(self):
-        return {'items': items}
-
-
 api.add_resource(Item, '/item/<string:name>')     # http://127.0.0.1:5000/student/alex
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
-app.run(port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
